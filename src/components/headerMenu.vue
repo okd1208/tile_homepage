fd9a3e0590fb172ef9c6c04f1f781b6b07c5e736
okd <y.okada.upd@gmail.com>
2022-02-14 16:28:10 +0900

    header active-title

--- src/components/headerMenu.vue
+++ src/components/headerMenu.vue
@@ -5,20 +5,18 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse class="mt-4 ml-lg-5" id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item class="ml-xl-3 mr-xl-3" @click="changeTopActive"><router-link to="/" class="nav-link text-dark">トップ<p class="subTitle" :class="{'activeSub':topActive}">Top</p></router-link></b-nav-item>
-            <b-nav-item class="ml-xl-3 mr-xl-3" @click="changeOverViewActive"><router-link to="/OverView" class="nav-link text-dark">会社情報<p class="subTitle" :class="{'activeSub':OVActive}">About</p></router-link></b-nav-item>
-            <b-nav-item class="ml-xl-3 mr-xl-3" @click="changeContentsActive"><router-link to="/contents" class="nav-link text-dark">事業内容<p class="subTitle" :class="{'activeSub':contentsActive}">Bisiness</p></router-link></b-nav-item>
-            <b-nav-item class="ml-xl-3 mr-xl-3" @click="changeRecruitActive"><router-link to="/recruit" class="nav-link text-dark">採用情報<p class="subTitle" :class="{'activeSub':recruitActive}">Recruit</p></router-link></b-nav-item>
-            <b-nav-item class="ml-xl-3 mr-xl-3" @click="changeTilesActive"><router-link to="/tiles" class="nav-link text-dark">タイル一覧<p class="subTitle" :class="{'activeSub':tilesActive}">tiles</p></router-link></b-nav-item>
-            <b-nav-item class="ml-xl-3 mr-xl-3" @click="changeConstructionExampleActive"><router-link to="/constructionExample" class="nav-link text-dark">施工例<p class="subTitle" :class="{'activeSub':constructionExampleActive}">Example</p></router-link></b-nav-item>
+            <b-nav-item class="ml-xl-3 mr-xl-3"><router-link to="/" class="nav-link text-dark">トップ<p class="subTitle" :class="{'activeSub':$route.name === 'home'}">Top</p></router-link></b-nav-item>
+            <b-nav-item class="ml-xl-3 mr-xl-3"><router-link to="/OverView" class="nav-link text-dark">会社情報<p class="subTitle" :class="{'activeSub':$route.name === 'OverView'}">About</p></router-link></b-nav-item>
+            <b-nav-item class="ml-xl-3 mr-xl-3"><router-link to="/contents" class="nav-link text-dark">事業内容<p class="subTitle" :class="{'activeSub':$route.name === 'contents'}">Bisiness</p></router-link></b-nav-item>
+            <b-nav-item class="ml-xl-3 mr-xl-3"><router-link to="/recruit" class="nav-link text-dark">採用情報<p class="subTitle" :class="{'activeSub':$route.name === 'recruit'}">Recruit</p></router-link></b-nav-item>
+            <b-nav-item class="ml-xl-3 mr-xl-3"><router-link to="/tiles" class="nav-link text-dark">タイル一覧<p class="subTitle" :class="{'activeSub':$route.name === 'tiles'}">tiles</p></router-link></b-nav-item>
+            <b-nav-item class="ml-xl-3 mr-xl-3"><router-link to="/constructionExample" class="nav-link text-dark">施工例<p class="subTitle" :class="{'activeSub':$route.name === 'constructionExample'}">Example</p></router-link></b-nav-item>
             <b-nav-item class="ml-xl-3 mr-xl-3 d-sm-none"><router-link to="/contact" class="nav-link text-dark">お問い合わせ<p class="subTitle">Contact</p></router-link></b-nav-item>
-            <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
           </b-navbar-nav>
         </b-collapse>
         <ul class="navbar-nav d-none d-sm-block mt-5">
           <li class="nav-item">
             <router-link to="/contact" class="nav-link btn contactBtn"><span class="contactText">お問い合わせ</span>
-            <!-- <p class="subTitle">Contact</p> -->
             </router-link>
           </li>
         </ul>
@@ -31,63 +29,9 @@ export default {
   name: 'headerMenu',
   data: function () {
     return {
-      topActive: false,
-      OVActive: false,
-      contentsActive: false,
-      recruitActive: false,
-      tilesActive: false,
-      constructionExampleActive: false
     }
   },
   methods: {
-    changeTopActive () {
-      this.topActive = true
-      this.OVActive = false
-      this.contentsActive = false
-      this.recruitActive = false
-      this.tilesActive = false
-      this.constructionExampleActive = false
-    },
-    changeOverViewActive () {
-      this.topActive = false
-      this.OVActive = true
-      this.contentsActive = false
-      this.recruitActive = false
-      this.tilesActive = false
-      this.constructionExampleActive = false
-    },
-    changeContentsActive () {
-      this.topActive = false
-      this.OVActive = false
-      this.contentsActive = true
-      this.recruitActive = false
-      this.tilesActive = false
-      this.constructionExampleActive = false
-    },
-    changeRecruitActive () {
-      this.topActive = false
-      this.OVActive = false
-      this.contentsActive = false
-      this.recruitActive = true
-      this.tilesActive = false
-      this.constructionExampleActive = false
-    },
-    changeTilesActive () {
-      this.topActive = false
-      this.OVActive = false
-      this.contentsActive = false
-      this.recruitActive = false
-      this.tilesActive = true
-      this.constructionExampleActive = false
-    },
-    changeConstructionExampleActive () {
-      this.topActive = false
-      this.OVActive = false
-      this.contentsActive = false
-      this.recruitActive = false
-      this.tilesActive = false
-      this.constructionExampleActive = true
-    }
   }
 }
 </script>
